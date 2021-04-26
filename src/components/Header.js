dbf3b10760b271bdf373e1c2d2b371405111ef77
CHEWERN <CHEWERN@gmail.com>
2021-04-26 10:20:36 +0800

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from "./Button"
+import { useLocation } from 'react-router-dom'
+//useLocation is to control the display of "Add Task" button
+//such that when in the "About" page, this button is not visible
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red':'green'} 
-            text={showAdd ? 'Close': 'Add Task'} 
-            onClick={onAdd} />
+            {(location.pathname === '/') &&
+                (<Button color={showAdd ? 'red':'green'} 
+                text={showAdd ? 'Close': 'Add Task'} 
+                onClick={onAdd} />)
+            }
         </header>
     )
 }
